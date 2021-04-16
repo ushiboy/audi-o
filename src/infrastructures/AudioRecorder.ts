552af5e6d34f32265ed4ab46cc6c70c114decdaa
ushiboy <ushiboy.dev@gmail.com>
2021-04-16 22:24:19 +0900

    add analyser

--- src/infrastructures/AudioRecorder.ts
+++ src/infrastructures/AudioRecorder.ts
@@ -1,27 +1,37 @@
 export interface AudioRecorderInterface {
   start(): Promise<void>;
   stop(): Promise<Blob>;
+  getData(): Uint8Array;
 }
 
 export class AudioRecorder implements AudioRecorderInterface {
   chunks: Blob[];
   mediaRecorder: MediaRecorder | null;
+  analyser: AnalyserNode | null;
 
   constructor() {
     this.chunks = [];
     this.mediaRecorder = null;
+    this.analyser = null;
   }
 
   start(): Promise<void> {
+    this.chunks = [];
     return navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then((stream) => {
-      this.mediaRecorder = new MediaRecorder(stream);
-      this.mediaRecorder.ondataavailable = (e) => {
-        this.chunks.push(e.data);
-      };
-      this.mediaRecorder.start();
-    });
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const audioCtx = new AudioContext();
+        const source = audioCtx.createMediaStreamSource(stream);
+        this.analyser = audioCtx.createAnalyser();
+        this.analyser.fftSize = 2048;
+        source.connect(this.analyser);
+
+        this.mediaRecorder = new MediaRecorder(stream);
+        this.mediaRecorder.ondataavailable = (e) => {
+          this.chunks.push(e.data);
+        };
+        this.mediaRecorder.start();
+      });
   }
 
   async stop(): Promise<Blob> {
@@ -41,5 +51,15 @@ export class AudioRecorder implements AudioRecorderInterface {
       };
       this.mediaRecorder.stop();
     });
+  }
+
+  getData(): Uint8Array {
+    if (this.analyser == null) {
+      throw new Error('Recorder does not started');
+    }
+    const bufferLength = this.analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    this.analyser.getByteTimeDomainData(dataArray);
+    return dataArray;
   }
 }
