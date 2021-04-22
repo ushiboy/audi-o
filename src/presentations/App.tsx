d5cb965be6a5986471ec9acba8d8cef6de2de156
ushiboy <ushiboy.dev@gmail.com>
2021-04-22 11:24:49 +0900

    fix

--- src/presentations/App.tsx
+++ src/presentations/App.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-import { Recorder } from './Recorder';
 import {
   AudioRecorder,
   AudioRecorderInterface,
@@ -14,33 +14,34 @@ const App: React.FC = () => {
   const audioRef = useRef<AudioRecorderInterface>();
   const animationRef = useRef(0);
   const [audioUrls, setAudioUrls] = useState<string[]>([]);
+  const [recording, setRecording] = useState(false);
 
   const animate = () => {
     const canvas = canvasRef.current;
-    const audio = audioRef.current
-    animationRef.current = requestAnimationFrame(animate)
+    const audio = audioRef.current;
+    animationRef.current = requestAnimationFrame(animate);
     if (canvas == null || audio == null) {
       return;
     }
-    const {width, height } = canvas;
+    const { width, height } = canvas;
     const ctx = canvas.getContext('2d')!;
-    ctx.fillStyle = 'rgb(200, 200, 200)';
+    ctx.fillStyle = '#e0f7fa';
     ctx.fillRect(0, 0, width, height);
 
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#4caf50';
 
     ctx.beginPath();
     try {
       const bufferLength = audio.getBufferSize();
-      const sliceWidth = width * 1.0 / bufferLength;
+      const sliceWidth = (width * 1.0) / bufferLength;
       const dataArray = audio.getData();
       let x = 0;
 
-      for(let i = 0; i < bufferLength; i++) {
+      for (let i = 0; i < bufferLength; i++) {
         const v = dataArray[i] / 128.0;
-        const y = v * height/2;
-        if(i === 0) {
+        const y = (v * height) / 2;
+        if (i === 0) {
           ctx.moveTo(x, y);
         } else {
           ctx.lineTo(x, y);
@@ -48,9 +49,9 @@ const App: React.FC = () => {
         x += sliceWidth;
       }
 
-      ctx.lineTo(canvas.width, canvas.height/2);
+      ctx.lineTo(canvas.width, canvas.height / 2);
       ctx.stroke();
-    } catch(e) {
+    } catch (e) {
       // ignore
     }
   };
@@ -65,17 +66,31 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <Recorder
-        onRecordClick={() => {
-          audioRef.current?.start();
-        }}
-        onStopClick={async () => {
-          const blob = await audioRef.current?.stop();
-          const u = URL.createObjectURL(blob);
-          setAudioUrls((s) => [...s, u]);
-        }}
-      />
-      <canvas ref={canvasRef} style={{height: 60, width: 180}} />
+      <div>
+        <Button
+          color="primary"
+          disabled={recording}
+          onClick={() => {
+            setRecording(true);
+            audioRef.current?.start();
+          }}
+        >
+          Record
+        </Button>
+        <Button
+          color="primary"
+          disabled={!recording}
+          onClick={async () => {
+            const blob = await audioRef.current?.stop();
+            const u = URL.createObjectURL(blob);
+            setAudioUrls((s) => [...s, u]);
+            setRecording(false);
+          }}
+        >
+          Stop
+        </Button>
+      </div>
+      <canvas ref={canvasRef} style={{ height: 120, width: 640 }} />
       {audioUrls.map((u, i) => (
         <Card key={i}>
           <div>
