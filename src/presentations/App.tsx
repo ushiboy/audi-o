e2fcd613b2d6ddd204af914e223ef53ab509d840
ushiboy <ushiboy.dev@gmail.com>
2021-04-24 14:47:42 +0900

    separated into recording components

--- src/presentations/App.tsx
+++ src/presentations/App.tsx
@@ -1,108 +1,24 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { Button, Box, Container } from '@material-ui/core';
-import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
-import StopIcon from '@material-ui/icons/Stop';
+import React, { useState } from 'react';
+import { Container } from '@material-ui/core';
 
-import { AudioRecorderInterface } from '../infrastructures';
 import { AudioRecordData } from '../domains';
 
-import { useAudioRecorder } from './Context';
 import { CreateFileDialog } from './CreateFileDialog';
 import { AudioRecordCard } from './AudioRecordCard';
+import { Recorder } from './Recorder';
 
 const App: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const audioRef = useRef<AudioRecorderInterface>();
-  const animationRef = useRef(0);
   const [records, setRecords] = useState<AudioRecordData[]>([]);
   const [draftRecord, setDraftRecord] = useState<AudioRecordData | null>(null);
-  const [recording, setRecording] = useState(false);
-
-  const animate = () => {
-    animationRef.current = requestAnimationFrame(animate);
-    const canvas = canvasRef.current;
-    const audio = audioRef.current;
-    if (canvas == null || audio == null) {
-      return;
-    }
-    const { width, height } = canvas;
-    const ctx = canvas.getContext('2d');
-    if (ctx == null) {
-      return;
-    }
-    ctx.fillStyle = '#e0f7fa';
-    ctx.fillRect(0, 0, width, height);
-
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#4caf50';
-
-    ctx.beginPath();
-    try {
-      const bufferLength = audio.getBufferSize();
-      const sliceWidth = (width * 1.0) / bufferLength;
-      const dataArray = audio.getData();
-      let x = 0;
-
-      for (let i = 0; i < bufferLength; i++) {
-        const v = dataArray[i] / 128.0;
-        const y = (v * height) / 2;
-        if (i === 0) {
-          ctx.moveTo(x, y);
-        } else {
-          ctx.lineTo(x, y);
-        }
-        x += sliceWidth;
-      }
-
-      ctx.lineTo(width, height / 2);
-      ctx.stroke();
-    } catch (e) {
-      // ignore
-    }
-  };
-
-  const audioRecorder = useAudioRecorder();
-  useEffect(() => {
-    audioRef.current = audioRecorder;
-    return () => {
-      cancelAnimationFrame(animationRef.current);
-    };
-  }, [audioRecorder]);
-
   return (
     <>
       <Container maxWidth="sm">
-        <Box>
-          <canvas ref={canvasRef} style={{ height: 120, width: 640 }} />
-          <Button
-            variant="contained"
-            color="secondary"
-            disabled={recording}
-            startIcon={<KeyboardVoiceIcon />}
-            onClick={() => {
-              setRecording(true);
-              audioRef.current?.start();
-              animate();
-            }}
-          >
-            Record
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={!recording}
-            startIcon={<StopIcon />}
-            onClick={async () => {
-              const blob = await audioRef.current?.stop();
-              const url = URL.createObjectURL(blob);
-              setDraftRecord({ title: '', url });
-              setRecording(false);
-              cancelAnimationFrame(animationRef.current);
-            }}
-          >
-            Stop
-          </Button>
-        </Box>
+        <Recorder
+          onRecordedAudio={(blob) => {
+            const url = URL.createObjectURL(blob);
+            setDraftRecord({ title: '', url });
+          }}
+        />
         {records.map((r, i) => (
           <AudioRecordCard key={i} record={r} />
         ))}
---
+++ src/presentations/Recorder.tsx
@@ -0,0 +1,117 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { Button, Box } from '@material-ui/core';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import StopIcon from '@material-ui/icons/Stop';
+
+import { AudioRecorderInterface } from '../infrastructures';
+
+import { useAudioRecorder } from './Context';
+
+type Props = {
+  onRecordedAudio: (blob: Blob) => void;
+};
+
+export const Recorder: React.FC<Props> = ({ onRecordedAudio }: Props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const audioRef = useRef<AudioRecorderInterface>();
+  const animationRef = useRef(0);
+  const [recording, setRecording] = useState(false);
+
+  const animate = () => {
+    animationRef.current = requestAnimationFrame(animate);
+    drawAmplitude(canvasRef.current!, audioRef.current!);
+  };
+
+  const audioRecorder = useAudioRecorder();
+  useEffect(() => {
+    audioRef.current = audioRecorder;
+    resetAmplitude(canvasRef.current!);
+    return () => {
+      cancelAnimationFrame(animationRef.current);
+    };
+  }, [audioRecorder]);
+
+  return (
+    <Box>
+      <canvas ref={canvasRef} style={{ height: 120, width: 640 }} />
+      <Button
+        variant="contained"
+        color="secondary"
+        disabled={recording}
+        startIcon={<KeyboardVoiceIcon />}
+        onClick={() => {
+          setRecording(true);
+          audioRef.current?.start();
+          animate();
+        }}
+      >
+        Record
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={!recording}
+        startIcon={<StopIcon />}
+        onClick={async () => {
+          const blob = await audioRef.current?.stop();
+          setRecording(false);
+          cancelAnimationFrame(animationRef.current);
+          resetAmplitude(canvasRef.current!);
+          if (blob !== undefined) {
+            onRecordedAudio(blob);
+          }
+        }}
+      >
+        Stop
+      </Button>
+    </Box>
+  );
+};
+
+const resetAmplitude = (canvas: HTMLCanvasElement): void => {
+  const { width, height } = canvas;
+  const ctx = canvas.getContext('2d')!;
+  ctx.fillStyle = '#e0f7fa';
+  ctx.fillRect(0, 0, width, height);
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = '#4caf50';
+
+  ctx.beginPath();
+  ctx.moveTo(0, height / 2);
+  ctx.lineTo(width, height / 2);
+  ctx.stroke();
+};
+
+const drawAmplitude = (
+  canvas: HTMLCanvasElement,
+  audio: AudioRecorderInterface
+): void => {
+  const { width, height } = canvas;
+  const ctx = canvas.getContext('2d')!;
+  ctx.fillStyle = '#e0f7fa';
+  ctx.fillRect(0, 0, width, height);
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = '#4caf50';
+
+  ctx.beginPath();
+  const bufferLength = audio.getBufferSize();
+  const sliceWidth = (width * 1.0) / bufferLength;
+  const dataArray = audio.getData();
+  let x = 0;
+
+  for (let i = 0; i < bufferLength; i++) {
+    const v = dataArray[i] / 128.0;
+    const y = (v * height) / 2;
+    if (i === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineTo(x, y);
+    }
+    x += sliceWidth;
+  }
+
+  ctx.lineTo(width, height / 2);
+  ctx.stroke();
+};
