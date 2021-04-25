1ea1718bcebb1cc9198514ce45b78194f77f3e78
ushiboy <ushiboy.dev@gmail.com>
2021-04-25 10:42:05 +0900

    add confirmation function before deletion

--- src/presentations/App.tsx
+++ src/presentations/App.tsx
@@ -4,12 +4,16 @@ import { Container } from '@material-ui/core';
 import { AudioRecordData } from '../domains';
 
 import { CreateFileDialog } from './CreateFileDialog';
+import { DeleteFileDialog } from './DeleteFileDialog';
 import { AudioRecordCard } from './AudioRecordCard';
 import { Recorder } from './Recorder';
 
 const App: React.FC = () => {
   const [records, setRecords] = useState<AudioRecordData[]>([]);
   const [draftRecord, setDraftRecord] = useState<AudioRecordData | null>(null);
+  const [deletionTarget, setDeletionTarget] = useState<AudioRecordData | null>(
+    null
+  );
   return (
     <>
       <Container maxWidth="sm">
@@ -24,11 +28,7 @@ const App: React.FC = () => {
             key={i}
             record={r}
             onDeleteClick={(d) => {
-              setRecords((s) =>
-                s.filter(
-                  ({ title, url }) => !(title === d.title && url === d.url)
-                )
-              );
+              setDeletionTarget(d);
             }}
           />
         ))}
@@ -41,6 +41,22 @@ const App: React.FC = () => {
           }}
           onCancelCreate={() => {
             setDraftRecord(null);
+          }}
+        />
+      ) : null}
+      {deletionTarget !== null ? (
+        <DeleteFileDialog
+          record={deletionTarget}
+          onDeleteFile={(d) => {
+            setRecords((s) =>
+              s.filter(
+                ({ title, url }) => !(title === d.title && url === d.url)
+              )
+            );
+            setDeletionTarget(null);
+          }}
+          onCancelDelete={() => {
+            setDeletionTarget(null);
           }}
         />
       ) : null}
---
+++ src/presentations/DeleteFileDialog.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { AudioRecordData } from '../domains';
+
+type Props = {
+  record: AudioRecordData;
+  onDeleteFile: (record: AudioRecordData) => void;
+  onCancelDelete: () => void;
+};
+
+export const DeleteFileDialog: React.FC<Props> = ({
+  record,
+  onDeleteFile,
+  onCancelDelete,
+}: Props) => {
+  return (
+    <Dialog open={true}>
+      <DialogTitle>Delete Audio</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Do you want to delete the file?</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancelDelete} color="primary">
+          CANCEL
+        </Button>
+        <Button
+          startIcon={<DeleteIcon />}
+          onClick={() => {
+            onDeleteFile(record);
+          }}
+          color="primary"
+        >
+          DELETE
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
